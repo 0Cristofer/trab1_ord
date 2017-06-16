6e725321bc90e967f2c72d60d0298f8774eaae6a
Cristofer Oswald <cristoferoswald@gmail.com>
2017-06-15 21:51:44 -0300

    Commit inicial

---
+++ main.c
@@ -0,0 +1,14 @@
+/* Arquivo principal da aplicação, organiza a interface e executa as funções
+   requisitadas
+   Data: 15/06/2017
+   Autores: Bruno Cesar, Cristofer Oswald */
+
+#include <stdio.h>
+
+#include "ord.h"
+
+
+int main(){
+  printf("Arquivo de entrada: %s", DATA_FILE);
+  return 0;
+}
---
+++ ord.h
@@ -0,0 +1,17 @@
+/* Arquivo header que contém definições para o programa
+   Data: 15/06/2017
+   Autores: Bruno Cesar, Cristofer Oswald */
+
+#ifndef ORD_H
+#define ORD_H
+
+#define DATA_FILE "dados-inline.txt"
+
+typedef struct{
+  long inscricao;
+  char* nome;
+  char* curso;
+  int socre;
+}registro;
+
+#endif /* ORD_H */
